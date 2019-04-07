a9ffa04cdf7cbd1a6ad6ca41f989ca933f81e4e1
volodymyr <volodymyr.klymkiv@gmail.com>
2019-04-07 22:14:43 +0300

    4 update

---
+++ lesson14/task1/js/script.js
@@ -0,0 +1,14 @@
+for (var a = 0; a < 10; a++) {
+    console.log(a * a);
+}
+
+
+var a = prompt();
+var b = prompt();
+if (a > b) {
+    console.log(a);
+} else if (a < b){
+    console.log(b);
+} else {
+    console.log('Рівність');
+};
